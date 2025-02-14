fd2d7e4c9fece11a3ad1147bd7dc3862e889d504
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-02-14 17:57:57 +1100

    Add basic VBSP write functions.

---
+++ Compilers/VBSP/IO/Write.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VBSP.IO
+{
+    public class BSPWriter
+    {
+        public BinaryWriter BinaryWriter;
+        private readonly int version;
+
+        public BSPWriter(string filePath, int version = 21)
+        {
+            BinaryWriter = new BinaryWriter(File.Open(filePath, FileMode.Create));
+            this.version = version;
+        }
+        
+        /// <summary>
+        /// Writes an input to BSP.
+        /// </summary>
+        public void Write(object input)
+        {
+            // Write version first (int)
+            BinaryWriter.Write(version);
+
+            if (input is string str)
+            {
+                // Write string to binary
+                BinaryWriter.Write(str);
+            }
+            else if (input is int integer)
+            {
+                // Write integer to binary
+                BinaryWriter.Write(integer);
+            }
+            // Unsupported Type
+            else
+            {
+                throw new InvalidOperationException("Unsupported type");
+            }
+
+            // Add to file
+            BinaryWriter.Flush();
+        }
+    }
+}
--- Compilers/VBSP/Program.cs
+++ Compilers/VBSP/Program.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using VBSP.IO;
 class Program
 {
     static void Main(string[] args)
     {
         // Entry point of the application
         Console.WriteLine("Hello, World!");
+
+        // By default, write the BSP to the location of vbsp.exe
+        BSPWriter writer = new BSPWriter("./bsp_test.bsp");
+        writer.Write("Test Binary Write.");
 
         // Command-line arguments using the 'args' parameter
         if (args.Length > 0)
