ffe65677d40859677a0ea0527799881b1914673f
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-02-17 18:53:17 +1100

    Update BSP namespaces.

---
+++ FileFormats/BSP/Read.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileFormats.BSP
+{
+    public class BSPReader
+    {
+    }
+}
--- FileFormats/BSP/Write.cs
+++ FileFormats/BSP/Write.cs
@@ -3,7 +3,7 @@ using System;
 using System.IO;
 using System.Text;
 
-namespace VBSP.IO
+namespace FileFormats.BSP
 {
     public class BSPWriter : IDisposable
     {
