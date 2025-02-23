43a8862441b91d7f4dd6bd286aeef6a5ce3c78d7
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-02-24 10:16:52 +1100

    Stop throwing exception when OpenGL Shader Uniform is not found.

--- Engine/Rendering/OpenGL/OpenGLShader.cs
+++ Engine/Rendering/OpenGL/OpenGLShader.cs
@@ -48,7 +48,8 @@ namespace SourceRewrite.Rendering.OpenGL
             int location = _gl.GetUniformLocation(_handle, name);
             if (location == -1) // If GetUniformLocation returns -1 the uniform is not found.
             {
-                throw new ArgumentException($"Uniform {name} not found in shader");
+                Console.WriteLine($"Uniform {name} not found in shader");
+                return;
             }
 
             _gl.UseProgram(_handle); // Bind our Shader to allow uniform changes
