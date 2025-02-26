d3cb9cee78522acdc4872491826c646109a06741
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-02-26 23:12:55 +1100

    Update BSP Geometry to have normals.

--- FileFormats/BSP/Lumps/LumpDefinitions.cs
+++ FileFormats/BSP/Lumps/LumpDefinitions.cs
@@ -9,60 +9,58 @@
 
             //                 Data Type       Data          Lump Type
             new LumpDefinition(typeof(string), "metal_sheet.vmt", LumpType.LUMP_MATERIAL), // Materials
-            new LumpDefinition(typeof(float[]), new float[] {     // Front face
-                -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,  // Bottom-left
-                0.5f, -0.5f, -0.5f, 1.0f, 0.0f,  // Bottom-right
-                0.5f, 0.5f, -0.5f, 1.0f, 1.0f,  // Top-right
-                -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,  // Top-left
+new LumpDefinition(typeof(float[]), new float[] {
+    // Front face - Normal: (0, 0, -1)
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, -1.0f,  0.0f, 0.0f,  // Bottom-left
+     0.5f, -0.5f, -0.5f,  0.0f, 0.0f, -1.0f,  1.0f, 0.0f,  // Bottom-right
+     0.5f,  0.5f, -0.5f,  0.0f, 0.0f, -1.0f,  1.0f, 1.0f,  // Top-right
+    -0.5f,  0.5f, -0.5f,  0.0f, 0.0f, -1.0f,  0.0f, 1.0f,  // Top-left
 
-                // Back face
-                -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,  // Bottom-left
-                0.5f, -0.5f, 0.5f, 1.0f, 0.0f,  // Bottom-right
-                0.5f, 0.5f, 0.5f, 1.0f, 1.0f,  // Top-right
-                -0.5f, 0.5f, 0.5f, 0.0f, 1.0f,  // Top-left
+    // Back face - Normal: (0, 0, 1)
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 1.0f,  0.0f, 0.0f,  // Bottom-left
+     0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 1.0f,  1.0f, 0.0f,  // Bottom-right
+     0.5f,  0.5f,  0.5f,  0.0f, 0.0f, 1.0f,  1.0f, 1.0f,  // Top-right
+    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f, 1.0f,  0.0f, 1.0f,  // Top-left
 
-                // Left face
-                -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,  // Bottom-left
-                -0.5f, 0.5f, -0.5f, 1.0f, 0.0f,  // Top-left
-                -0.5f, 0.5f, 0.5f, 1.0f, 1.0f,  // Top-right
-                -0.5f, -0.5f, 0.5f, 0.0f, 1.0f,  // Bottom-right
+    // Left face - Normal: (-1, 0, 0)
+    -0.5f, -0.5f, -0.5f, -1.0f, 0.0f, 0.0f,  0.0f, 0.0f,  // Bottom-left
+    -0.5f,  0.5f, -0.5f, -1.0f, 0.0f, 0.0f,  1.0f, 0.0f,  // Top-left
+    -0.5f,  0.5f,  0.5f, -1.0f, 0.0f, 0.0f,  1.0f, 1.0f,  // Top-right
+    -0.5f, -0.5f,  0.5f, -1.0f, 0.0f, 0.0f,  0.0f, 1.0f,  // Bottom-right
 
-                // Right face
-                0.5f, -0.5f, -0.5f, 0.0f, 0.0f,  // Bottom-left
-                0.5f, 0.5f, -0.5f, 1.0f, 0.0f,  // Top-left
-                0.5f, 0.5f, 0.5f, 1.0f, 1.0f,  // Top-right
-                0.5f, -0.5f, 0.5f, 0.0f, 1.0f,  // Bottom-right
+    // Right face - Normal: (1, 0, 0)
+     0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 0.0f,  0.0f, 0.0f,  // Bottom-left
+     0.5f,  0.5f, -0.5f,  1.0f, 0.0f, 0.0f,  1.0f, 0.0f,  // Top-left
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.0f,  1.0f, 1.0f,  // Top-right
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f, 0.0f,  0.0f, 1.0f,  // Bottom-right
 
-                // Top face
-                -0.5f, 0.5f, -0.5f, 0.0f, 0.0f,  // Bottom-left
-                0.5f, 0.5f, -0.5f, 1.0f, 0.0f,  // Bottom-right
-                0.5f, 0.5f, 0.5f, 1.0f, 1.0f,  // Top-right
-                -0.5f, 0.5f, 0.5f, 0.0f, 1.0f,  // Top-left
+    // Top face - Normal: (0, 1, 0)
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.0f,  0.0f, 0.0f,  // Bottom-left
+     0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.0f,  1.0f, 0.0f,  // Bottom-right
+     0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 0.0f,  1.0f, 1.0f,  // Top-right
+    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 0.0f,  0.0f, 1.0f,  // Top-left
 
-                // Bottom face
-                -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,  // Bottom-left
-                0.5f, -0.5f, -0.5f, 1.0f, 0.0f,  // Bottom-right
-                0.5f, -0.5f, 0.5f, 1.0f, 1.0f,  // Top-right
-                -0.5f, -0.5f, 0.5f, 0.0f, 1.0f   // Top-left }, Lump.LumpType.LUMP_VERTEXES), // Vertices
+    // Bottom face - Normal: (0, -1, 0)
+    -0.5f, -0.5f, -0.5f,  0.0f, -1.0f, 0.0f,  0.0f, 0.0f,  // Bottom-left
+     0.5f, -0.5f, -0.5f,  0.0f, -1.0f, 0.0f,  1.0f, 0.0f,  // Bottom-right
+     0.5f, -0.5f,  0.5f,  0.0f, -1.0f, 0.0f,  1.0f, 1.0f,  // Top-right
+    -0.5f, -0.5f,  0.5f,  0.0f, -1.0f, 0.0f,  0.0f, 1.0f   // Top-left
+}, LumpType.LUMP_VERTEXES), // Vertices
 
-            }, LumpType.LUMP_VERTEXES), // Vertices
-            new LumpDefinition(typeof(uint[]), new uint[] {     // Front face
+new LumpDefinition(typeof(uint[]), new uint[] {
+    // Front face
     0, 1, 2,  0, 2, 3,
-
     // Back face
     4, 5, 6,  4, 6, 7,
-
     // Left face
     8, 9, 10,  8, 10, 11,
-
     // Right face
     12, 13, 14,  12, 14, 15,
-
     // Top face
     16, 17, 18,  16, 18, 19,
-
     // Bottom face
-    20, 21, 22,  20, 22, 23 }, LumpType.LUMP_INDICES), // Indices
+    20, 21, 22,  20, 22, 23
+}, LumpType.LUMP_INDICES), // Indices
             new LumpDefinition(
     typeof(string[]),
     new string[]
