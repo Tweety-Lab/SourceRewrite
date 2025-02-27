f30a7638084f60d3458ace48ca97f9ba672af3dd
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-02-27 20:02:37 +1100

    Make PointLight animate in a sphere.

--- Engine/Components/Lighting/PointLight.cs
+++ Engine/Components/Lighting/PointLight.cs
@@ -10,18 +10,22 @@ namespace SourceRewrite.Components.Lighting
 {
     public class PointLight : GameComponent
     {
-        public Vector3 Color { get; set; } = new Vector3(1, 1, 1);
+        public Vector3 Color { get; set; } = new Vector3(1f, 1f, 1);
         public float Intensity { get; set; } = 1.0f;
+
+        private int circleRadius = 7;
 
         // Update Shader Uniforms
         public override void Update(float deltaTime)
         {
             // Animate the light moving in a circle
             GameObject.Transform.Position = new Vector3(
-                (float)Math.Cos(Environment.TickCount / 1000.0f) * 5, // X
-                GameObject.Transform.Position.Y,                      // Y
-                (float)Math.Sin(Environment.TickCount / 1000.0f) * 5  // Z
+                (float)Math.Cos(Environment.TickCount / 700.0f) * circleRadius, // X
+                (float)Math.Cos(Environment.TickCount / 700.0f) * circleRadius, // Y
+                (float)Math.Sin(Environment.TickCount / 700.0f) * circleRadius  // Z
             );
+
+            // Animate the light moving in a sphere
 
             // Update Uniforms
             foreach (Shader shader in Shader.Shaders)
