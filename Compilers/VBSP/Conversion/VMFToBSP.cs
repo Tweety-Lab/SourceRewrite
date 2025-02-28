08a7f63ca81bdc64d9b8262855490a43926e041f
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-03-01 00:47:10 +1100

    Move VBSP Functions from Program.cs to VMFToBSP.cs.

---
+++ Compilers/VBSP/Conversion/VMFToBSP.cs
@@ -0,0 +1,55 @@
+﻿using FileFormats.BSP;
+using FileFormats.VMF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VBSP.Conversion
+{
+    public static class VMFToBSP
+    {
+        /// <summary>
+        /// Returns a BSPFormat object compiled from a VMF file.
+        /// </summary>
+        public static BSPFormat CompileVMF(string contents)
+        {
+            // Load the VMF file
+            VMFFormat VMF = new VMFFormat(contents);
+
+            BSPFormat outputBSP = new BSPFormat();
+
+            // Set the BSP header
+            outputBSP.Header.mapRevision = VMF.VersionInfo.MapVersion;
+
+            string[] entities = new string[VMF.Entities.Count];
+
+            // Convert entities to GameObject strings
+            // Right now this is just entity = component
+            int i = 0;
+            foreach (Entity vmfEntity in VMF.Entities)
+            {
+                string entityString = @$"
+            GameObject{i} {{
+                position ""{vmfEntity.Origin.X} {vmfEntity.Origin.Z} {vmfEntity.Origin.Y}""
+                rotation ""0 0 0""
+                scale ""1 1 1""
+                GameComponents {{
+                    {ClassConversion.ClassMap[$"{vmfEntity.ClassName}"]} {{
+
+                    }}
+                }}
+            }}";
+
+                entities[i] = entityString;
+
+                i++;
+            }
+
+            outputBSP.SetLumpData(LumpType.LUMP_GAME_OBJECTS, entities);
+
+            return outputBSP;
+        }
+    }
+}
--- Compilers/VBSP/Program.cs
+++ Compilers/VBSP/Program.cs
@@ -18,40 +18,8 @@ class Program
         // Read the VMF file
         string contents = File.ReadAllText(vmfPath);
 
-        // Load the VMF file
-        VMFFormat VMF = new VMFFormat(contents);
-
-        BSPFormat outputBSP = new BSPFormat();
-
-        // Set the BSP header
-        outputBSP.Header.mapRevision = VMF.VersionInfo.MapVersion;
-
-        string[] entities = new string[VMF.Entities.Count];
-
-        // Convert entities to GameObject strings
-        // Right now this is just entity = component
-        int i = 0;
-        foreach (Entity vmfEntity in VMF.Entities)
-        {
-            string entityString = @$"
-            GameObject{i} {{
-                position ""{vmfEntity.Origin.X} {vmfEntity.Origin.Z} {vmfEntity.Origin.Y}""
-                rotation ""0 0 0""
-                scale ""1 1 1""
-                GameComponents {{
-                    {ClassConversion.ClassMap[$"{vmfEntity.ClassName}"]} {{
-
-                    }}
-                }}
-            }}";
-
-            entities[i] = entityString;
-
-            i++;
-        }
-
-        outputBSP.SetLumpData(LumpType.LUMP_GAME_OBJECTS, entities);
-
+        // Compile the BSP
+        BSPFormat compiledBSP = VMFToBSP.CompileVMF(contents);
 
         // Set output BSP file path
         string bspOutputPath = "./bsp_test.bsp";
@@ -59,7 +27,7 @@ class Program
         // Write BSP
         using (BSPWriter writer = new BSPWriter(bspOutputPath, vmfPath))
         {
-            writer.WriteToMap(outputBSP);
+            writer.WriteToMap(compiledBSP);
         }
 
         Console.WriteLine($"BSP successfully written to: {bspOutputPath}");
