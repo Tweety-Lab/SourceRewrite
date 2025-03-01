66e2bf86be87e0e02755f8024370a42f31152ec2
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-03-02 00:05:49 +1100

    Port info_player_start.

--- Compilers/VBSP/Conversion/ClassConversion.cs
+++ Compilers/VBSP/Conversion/ClassConversion.cs
@@ -7,7 +7,8 @@
         {
             // Valve Class Name | Our Class Name
             { "light",          "SourceRewrite_Components_PointLight" },
-            { "prop_static",    "SourceRewrite_Components_MeshRenderer" }
+            { "prop_static",    "SourceRewrite_Components_MeshRenderer" },
+            { "info_player_start", "SourceRewrite_Components_CameraSpawn" }
         };
     }
 }
---
+++ Engine/Components/CameraSpawn.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceRewrite.Components
+{
+    public class CameraSpawn : GameComponent
+    {
+        public override void Start()
+        {
+            // Set the active cameras position to the spawn position
+            Camera.ActiveCamera.GameObject.Transform.Position = GameObject.Transform.Position;
+        }
+    }
+}
