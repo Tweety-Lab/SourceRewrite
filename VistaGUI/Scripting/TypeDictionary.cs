86e6f753ade7ad24b9c0d42a2fe801e9e568b8b8
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-03-13 22:08:09 +1100

    Cleanup Vista TypeDictionary.

--- VistaGUI/Scripting/TypeDictionary.cs
+++ VistaGUI/Scripting/TypeDictionary.cs
@@ -1,24 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using VistaGUI.Scripting.References;
+﻿using VistaGUI.Scripting.References;
 
 namespace VistaGUI.Scripting
 {
-    // A dictionary mapping element types (HTML tags) to corresponding C# types.
+    
     public static class TypeDictionary
     {
-        public static readonly Dictionary<string, Type> Types = new Dictionary<string, Type>
+        // A dictionary mapping element types (HTML tags) to corresponding C# types.
+        public static readonly Dictionary<string, Type> Types = new()
         {
-            { "p", typeof(TextReference) },
-            { "b", typeof(TextReference) },
-            { "i", typeof(TextReference) },
-            { "span", typeof(TextReference) },
-            { "h1", typeof(TextReference) },
-            { "h2", typeof(TextReference) },
-            { "h3", typeof(TextReference) }
+            // Text elements
+            ["p"] = typeof(TextReference),
+            ["b"] = typeof(TextReference),
+            ["i"] = typeof(TextReference),
+            ["span"] = typeof(TextReference),
+            ["h1"] = typeof(TextReference),
+            ["h2"] = typeof(TextReference),
+            ["h3"] = typeof(TextReference)
         };
     }
 }
