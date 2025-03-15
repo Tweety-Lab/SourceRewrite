635138766f718c4855785cc70b23e011b0c98087
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-03-16 01:41:23 +1100

    Add comments to Editor GUI Component.

--- Editor/Components/GUI/Editor.cs
+++ Editor/Components/GUI/Editor.cs
@@ -33,23 +33,28 @@ namespace Editor.Components.GUI
             canvas.RegisterEvent("OpenVDC", () => Process.Start(new ProcessStartInfo("https://developer.valvesoftware.com/wiki/Main_Page") { UseShellExecute = true }));
 
             bool lightEditorOpen = false;
+
+            // Open Light Editor if it isn't already opened
             canvas.RegisterEvent("OpenLightEditor", OpenLightEditor);
             void OpenLightEditor()
             {
                 if (!lightEditorOpen)
                 {
+                    // Create gameobject to house Light Editor GUICanvas
                     lightEditorObject = new GameObject();
 
+                    // Add Light Edtior GUICanvas to gameobject
                     LightEditorCanvas lightEditorCanvas = new LightEditorCanvas();
-
                     lightEditorObject.AddComponent(lightEditorCanvas);
 
+                    // Start GUI
                     lightEditorCanvas.Start();
 
                     lightEditorOpen = true;
                 }
             }
 
+            // Close Light Editor if it isn't already closed
             canvas.RegisterEvent("CloseLightEditor", CloseLightEditor);
             void CloseLightEditor()
             {
