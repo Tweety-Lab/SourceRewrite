519deb21542d9caed1a00547990375b4b468f753
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-03-16 19:32:07 +1100

    Add placeholder file_explorer GUI.

--- Editor/Components/GUI/Editor.cs
+++ Editor/Components/GUI/Editor.cs
@@ -8,6 +8,7 @@ using System;
 using System.Numerics;
 using VistaGUI.Scripting.References;
 using SourceRewrite.Maps;
+using SourceRewrite.Components.GUI;
 
 namespace Editor.Components.GUI
 {
@@ -64,17 +65,32 @@ namespace Editor.Components.GUI
                 }
             });
 
+            // Unload Map
+            canvas.RegisterEvent("CloseMap", () => MapSystem.UnloadMap());
+
+            // Play Buttons
+            canvas.RegisterEvent("PlayGame", () => Process.Start("Engine.exe")); // Just start Engine.exe as it loads game.dll
+
+            // Start Engine.exe with the current map as an argument
             canvas.RegisterEvent("PlayCurrentMap", () =>
             {
                 string mapFileName = MapSystem.CurrentMap.BSPFilePath.Split('/').Last(); // Get the file name of the map
                 Process.Start("Engine.exe", $"-map {mapFileName}"); // Start the process with the map argument
             });
 
-            // Unload Map
-            canvas.RegisterEvent("CloseMap", () => MapSystem.UnloadMap());
+            // Open a map in the editor
+            canvas.RegisterEvent("OpenMap", () =>
+            {
+                // Create gameobject to house fileExplorer GUICanvas
+                GameObject fileExplorerObject = new GameObject();
 
-            // Play Buttons
-            canvas.RegisterEvent("PlayGame", () => Process.Start("Engine.exe")); // Just start Engine.exe as it loads game.dll
+                // Add GUICanvas to gameobject
+                FileExplorerCanvas fileExplorerCanvas = new FileExplorerCanvas();
+                fileExplorerObject.AddComponent(fileExplorerCanvas);
+
+                // Start GUI
+                fileExplorerCanvas.Start();
+            });
         }
 
         public override void Update()
---
+++ Engine/Components/GUI/FileExplorerCanvas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceRewrite.Components.GUI
+{
+    /// <summary>
+    /// Placeholder FileExplorer GUI Element
+    /// </summary>
+    public class FileExplorerCanvas : GameComponent
+    {
+        private GUICanvas canvas;
+
+        public override void Start()
+        {
+            canvas = new GUICanvas();
+            canvas.IsTransparent = true;
+            canvas.PanelName = "elements/file_explorer/file_explorer.html";
+
+            GameObject.AddComponent(canvas);
+
+            // HACK: Manually start the canvas component
+            canvas.Start();
+        }
+    }
+}
