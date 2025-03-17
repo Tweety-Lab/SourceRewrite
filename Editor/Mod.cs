c03d164bcf2d1c3c195ddc46a7e61e7c156518c6
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-03-17 18:36:59 +1100

    Move Editor init code to IMod Entry Point.

--- Editor/Mod.cs
+++ Editor/Mod.cs
@@ -1,9 +1,14 @@
-﻿using SourceRewrite.Modding;
+﻿using Editor.Components.GUI;
+using Editor.Components;
+using SourceRewrite.Maps;
+using SourceRewrite.Modding;
+using SourceRewrite.Objects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SourceRewrite.Components;
 
 namespace Editor
 {
@@ -15,7 +20,26 @@ namespace Editor
         // Runs once on Mod Load
         public void OnLoad()
         {
-            
+            // Dont run GameObject start logic in our maps
+            Map.StartGameObjectsOnMapLoad = false;
+
+            // Load the Editor GUI
+            GameObject editorGUIObject = new GameObject();
+            EditorCanvas editorGUIComponent = new EditorCanvas();
+            editorGUIObject.AddComponent(editorGUIComponent);
+
+            // Add Editor GUI as global component
+            Map.GlobalGameObjects.Add(editorGUIObject);
+
+            // Load Editor Camera
+            GameObject editorCameraObject = new GameObject();
+            Camera editorCamera = new Camera();
+            EditorCameraController editorCameraController = new EditorCameraController();
+            editorCameraObject.AddComponent(editorCameraController);
+            editorCameraObject.AddComponent(editorCamera);
+
+            // Add Editor Camera as global component
+            Map.GlobalGameObjects.Add(editorCameraObject);
         }
 
         // Runs once on Mod Unload
--- Editor/Program.cs
+++ Editor/Program.cs
@@ -13,30 +13,8 @@ namespace Editor
     {
         static void Main(string[] args)
         {
-            // Load Editor as Mod
+            // Load Editor as Mod by default
             ModSystem.LoadModAssembly("Editor.dll");
-
-            // Dont run GameObject start logic in our maps
-            Map.StartGameObjectsOnMapLoad = false;
-
-            // Load the Editor GUI
-            GameObject editorGUIObject = new GameObject();
-            EditorCanvas editorGUIComponent = new EditorCanvas();
-            editorGUIObject.AddComponent(editorGUIComponent);
-
-            // Add Editor GUI as global component
-            Map.GlobalGameObjects.Add(editorGUIObject);
-
-            // Load Editor Camera
-            GameObject editorCameraObject = new GameObject();
-            Camera editorCamera = new Camera();
-            EditorCameraController editorCameraController = new EditorCameraController();
-            editorCameraObject.AddComponent(editorCameraController);
-            editorCameraObject.AddComponent(editorCamera);
-
-            // Add Editor Camera as global component
-            Map.GlobalGameObjects.Add(editorCameraObject);
-
 
             // Create our main Game Window
             new GameWindow(new Vector2(800, 500), "Game Window");
