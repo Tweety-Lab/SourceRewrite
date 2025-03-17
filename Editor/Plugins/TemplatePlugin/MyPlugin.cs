41643e6e7878246069047b8fa8f7ca8667aa3b72
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-03-18 01:53:24 +1100

    Added TemplatePlugin project.

---
+++ Editor/Plugins/TemplatePlugin/MyPlugin.cs
@@ -0,0 +1,19 @@
+﻿using SourceRewrite.Modding;
+
+namespace TemplatePlugin
+{
+    public class MyPlugin : IMod
+    {
+        // Runs when the mod is loaded
+        public void OnLoad()
+        {
+            Console.WriteLine("Loaded MyPlugin!");
+        }
+
+        // Runs when the mod is unloaded
+        public void  OnUnload()
+        {
+            Console.WriteLine("Shutting Down MyPlugin...");
+        }
+    }
+}
