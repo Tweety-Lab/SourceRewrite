b4b496022f62b6d64bb4022eb862a5d924837a89
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-03-17 22:23:24 +1100

    Add VistaListItem element.

---
+++ VistaGUI/Scripting/Elements/VistaListItem.cs
@@ -0,0 +1,43 @@
+﻿using VistaGUI.Scripting.Elements;
+
+namespace VistaGUI.Scripting.References
+{
+    /// <summary>
+    /// Generic reference to List Item (LI) Elements.
+    /// </summary>
+    [VistaElement("li")]
+    public class VistaListItem : VistaElement
+    {
+        /// <summary>
+        /// Text Content of the List Item.
+        /// </summary>
+        public string TextContent
+        {
+            get => GetProperty("textContent");
+            set => SetProperty("textContent", value);
+        }
+
+        /// <summary>
+        /// Determines if the List Item is marked as selected (for example, in a checklist or ordered list).
+        /// </summary>
+        public bool Selected
+        {
+            get => GetProperty("selected") == "true";
+            set => SetProperty("selected", value.ToString());
+        }
+
+        /// <summary>
+        /// Constructor for the List Item (LI) reference.
+        /// </summary>
+        public VistaListItem(string ID, VistaScriptingContext context) : base(ID, context) { }
+
+        /// <summary>
+        /// Remove this List Item (LI) from its parent List (UL/OL).
+        /// </summary>
+        public void Remove()
+        {
+            string currentContent = GetProperty("outerHTML");
+            SetProperty("outerHTML", string.Empty); // Remove the item by clearing its outer HTML
+        }
+    }
+}
