b54eca881d49dfb720a9db9d28ac730fd100e63c
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-03-23 18:21:18 +1100

    Remove old MeshExtensions class.

--- Engine/Rendering/RenderPasses.cs
+++ Engine/Rendering/RenderPasses.cs
@@ -93,38 +93,4 @@ namespace SourceRewrite.Rendering
             }
         }
     }
-
-    // Mesh Class with UV generation
-    public static class MeshExtensions
-    {
-        public static float[] GenerateDefaultUVs(this Mesh meshObject)
-        {
-            var uvs = new float[meshObject.Vertices.Length / 3 * 2];
-
-            for (int i = 0; i < meshObject.Vertices.Length; i += 12) // Assuming 4 vertices per face
-            {
-                var uv1 = new Vector2(0, 0);
-                var uv2 = new Vector2(1, 0);
-                var uv3 = new Vector2(1, 1);
-                var uv4 = new Vector2(0, 1);
-
-                int baseUvIndex = (i / 3) * 2;
-
-                // Apply UVs to each vertex in the face
-                uvs[baseUvIndex] = uv1.X;
-                uvs[baseUvIndex + 1] = uv1.Y;
-
-                uvs[baseUvIndex + 2] = uv2.X;
-                uvs[baseUvIndex + 3] = uv2.Y;
-
-                uvs[baseUvIndex + 4] = uv3.X;
-                uvs[baseUvIndex + 5] = uv3.Y;
-
-                uvs[baseUvIndex + 6] = uv4.X;
-                uvs[baseUvIndex + 7] = uv4.Y;
-            }
-
-            return uvs;
-        }
-    }
 }
