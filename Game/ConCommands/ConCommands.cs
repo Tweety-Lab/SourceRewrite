7d185c7ca49b2c809c698733b3b4bbe5ba91e848
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-03-24 21:59:49 +1100

    Add simple ConCommand to Game.

---
+++ Game/ConCommands/ConCommands.cs
@@ -0,0 +1,20 @@
+﻿using SourceRewrite;
+using SourceRewrite.Attributes;
+using SourceRewrite.Maps;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.ConCommands
+{
+    public static class ConCommands
+    {
+        [ConCommand("ping_client")]
+        static void Ping()
+        {
+            EngineConsole.Msg("Pong!");
+        }
+    }
+}
