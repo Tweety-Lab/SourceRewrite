cc1721ee7b62b44d83e6f5aed329491893476b7e
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-03-25 00:09:05 +1100

    Add "map" ConCommand.

---
+++ Engine/Entities/Console/ConCommands.cs
@@ -0,0 +1,20 @@
+﻿using SourceRewrite.Attributes;
+using SourceRewrite.Files;
+using SourceRewrite.Maps;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceRewrite.Entities.Console
+{
+    public static class ConCommands
+    {
+        [ConCommand("map")]
+        static void Map(string path)
+        {
+            MapSystem.LoadMap(FileSystem.GetMapPath(path));
+        }
+    }
+}
