06c243cc0ed9b0ed6ca47cfe21034058e17f0372
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-03-30 21:38:37 +1100

    Add Placeholder "cl_showfps" ConVar.

---
+++ Engine/Entities/GUI/BuiltIn/FPSCounter.cs
@@ -0,0 +1,47 @@
+﻿using SourceRewrite.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceRewrite.Entities.GUI.BuiltIn
+{
+    public class FPSCounter : ScreenspaceGUICanvas
+    {
+        private static FPSCounter _fpsCounterInstance;
+
+        [ConVar("cl_showfps")]
+        public static bool ShowFPS
+        {
+            get => _fpsCounterInstance != null; // Return true if instance exists
+            set
+            {
+                if (value && _fpsCounterInstance == null)
+                {
+                    // Create the instance of FPSCounter
+                    _fpsCounterInstance = new FPSCounter();
+                    _fpsCounterInstance.Parent = EntityManager.MapContainer;
+                    _fpsCounterInstance.Start();
+                }
+                else if (!value && _fpsCounterInstance != null)
+                {
+                    // Destroy the FPSCounter
+                    _fpsCounterInstance.DestroyDeferred();
+                    _fpsCounterInstance = null;
+                }
+            }
+        }
+
+        public override void Start()
+        {
+            PanelName = "utilities/fpscounter/counter.html";
+            base.Start();
+        }
+
+        public override void Update()
+        {
+            base.Update();
+        }
+    }
+}
