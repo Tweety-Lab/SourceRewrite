f0cb5f7eab5c032c463bdb003ce5aa9c46888a4d
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-04-02 00:08:51 +1100

    Create empy main menu GUI.

---
+++ Engine/Entities/GUI/MainMenuCanvas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceRewrite.Entities.GUI
+{
+    public class MainMenuCanvas : ScreenspaceGUICanvas
+    {
+        public override void Start()
+        {
+            PanelName = "menu/main-menu.html";
+            base.Start();
+        }
+    }
+}
