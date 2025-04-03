54bb1e3019ada0b5c18d8a6c2c74630a4a26aaf2
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-04-03 22:17:20 +1100

    Add VistaGUI PreProccesor Rule for auto adding custom element scripts.

--- VistaGUI/Preprocessor/PreProccesor.cs
+++ VistaGUI/Preprocessor/PreProccesor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace VistaGUI.Preprocessor
@@ -16,7 +17,42 @@ namespace VistaGUI.Preprocessor
         /// </summary>
         public static List<Func<string, string>> HTMLRules = new List<Func<string, string>>()
         {
+            // Rule: Auto include scripts for custom elements
+            html =>
+            {
+                bool modified = false;
+                StringBuilder scriptIncludes = new StringBuilder();
 
+                // Check for each custom element in the ElementMap
+                foreach (var element in ProcessorElements.ElementMap)
+                {
+                    string elementName = element.Key;
+                    string scriptPath = element.Value;
+
+                    // Check if the element exists in the HTML
+                    if (Regex.IsMatch(html, $@"<{elementName}[^>]*>", RegexOptions.IgnoreCase))
+                    {
+                        Console.WriteLine($"Adding script for: {elementName}");
+                        scriptIncludes.AppendLine($"<script src=\"file:///{scriptPath}\"></script>");
+                        modified = true;
+                    }
+                }
+
+                // Inject scripts before </body>
+                if (modified)
+                {
+                    if (html.Contains("</body>"))
+                    {
+                        html = Regex.Replace(html, @"</body>", scriptIncludes.ToString() + "</body>", RegexOptions.IgnoreCase);
+                    }
+                    else
+                    {
+                        html += scriptIncludes.ToString(); // Append at end if no </body> found
+                    }
+                }
+
+                return html;
+            }
         };
 
         public static string ProcessHTML(string html)
---
+++ VistaGUI/Preprocessor/ProcessorElements.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VistaGUI.Preprocessor
+{
+    public static class ProcessorElements
+    {
+        public static Dictionary<string, string> ElementMap = new Dictionary<string, string>()
+        {
+            // ELEMENT NAME, FILE PATH
+
+            // Panels
+            { "vista-panel", "elements/panels.js" },
+        };
+    }
+}
