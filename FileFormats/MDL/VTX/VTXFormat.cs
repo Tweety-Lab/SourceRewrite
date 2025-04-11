9e515c616f6ae3bb33b1786a9247e76bb339f055
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-04-11 18:11:15 +1000

    Correctly calculate VTX offsets.

--- FileFormats/MDL/VTX/VTXFormat.cs
+++ FileFormats/MDL/VTX/VTXFormat.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace FileFormats.MDL.VTX
 {
@@ -21,7 +19,6 @@ namespace FileFormats.MDL.VTX
 
         // Mesh Indices
         public List<int> MeshIndices;
-        
 
         // This is where the core of the index data is kept
         public List<VTXStripGroupHeader> StripGroupHeaders;
@@ -36,20 +33,17 @@ namespace FileFormats.MDL.VTX
             {
                 // Read the header
                 Header.Version = reader.ReadInt32();
-
                 Header.VertCacheSize = reader.ReadInt32();
                 Header.MaxBonesPerStrip = reader.ReadInt16();
                 Header.MaxBonesPerTri = reader.ReadInt16();
                 Header.MaxBonesPerVert = reader.ReadInt32();
-
                 Header.Checksum = reader.ReadInt32();
-
                 Header.NumLODs = reader.ReadInt32();
-
                 Header.MaterialReplacementListOffset = reader.ReadInt32();
-
                 Header.NumBodyParts = reader.ReadInt32();
                 Header.BodyPartOffset = reader.ReadInt32();
+
+                Console.WriteLine($"BodyPartOffset: {Header.BodyPartOffset}");
 
                 // Order here DOES matter
                 BodyPartHeaders = ReadBodyParts(reader);
@@ -74,13 +68,11 @@ namespace FileFormats.MDL.VTX
                 bodyPart.NumModels = reader.ReadInt32();
                 bodyPart.ModelOffset = reader.ReadInt32();
 
-                // Add the body part to the list
+                Console.WriteLine($"Body Part {i}: NumModels = {bodyPart.NumModels}, ModelOffset = {bodyPart.ModelOffset}");
                 VTXBodyParts.Add(bodyPart);
             }
 
-            // Return to original position
             reader.BaseStream.Seek(originalPos, SeekOrigin.Begin);
-
             return VTXBodyParts;
         }
 
@@ -92,20 +84,19 @@ namespace FileFormats.MDL.VTX
 
             for (int i = 0; i < BodyPartHeaders.Count; i++)
             {
-                reader.BaseStream.Seek(BodyPartHeaders[i].ModelOffset, SeekOrigin.Begin);
+                // Compute absolute offset: BodyPartOffset + ModelOffset
+                long absoluteModelOffset = Header.BodyPartOffset + BodyPartHeaders[i].ModelOffset;
+                reader.BaseStream.Seek(absoluteModelOffset, SeekOrigin.Begin);
 
                 VTXModelHeader model = new VTXModelHeader();
                 model.NumLODs = reader.ReadInt32();
                 model.LodOffset = reader.ReadInt32();
 
+                Console.WriteLine($"Model Header {i}: NumLODs = {model.NumLODs}, LodOffset = {model.LodOffset}");
                 VTXModels.Add(model);
-
-                Console.WriteLine($"Model {i}: NumLODs = {model.NumLODs}, LodOffset = {model.LodOffset}");
             }
 
-            // return to original position
             reader.BaseStream.Seek(originalPos, SeekOrigin.Begin);
-
             return VTXModels;
         }
 
@@ -113,25 +104,24 @@ namespace FileFormats.MDL.VTX
         public List<VTXModelLODHeader> ReadModelLODs(BinaryReader reader)
         {
             int originalPos = (int)reader.BaseStream.Position;
-
             List<VTXModelLODHeader> VTXModelLODs = new List<VTXModelLODHeader>();
+
             for (int i = 0; i < ModelHeaders.Count; i++)
             {
-                reader.BaseStream.Seek(ModelHeaders[i].LodOffset, SeekOrigin.Begin);
+                // Compute absolute offset: BodyPartOffset + ModelOffset + LodOffset
+                long absoluteLodOffset = Header.BodyPartOffset + BodyPartHeaders[i].ModelOffset + ModelHeaders[i].LodOffset;
+                reader.BaseStream.Seek(absoluteLodOffset, SeekOrigin.Begin);
 
                 VTXModelLODHeader modelLOD = new VTXModelLODHeader();
-
                 modelLOD.NumMeshes = reader.ReadInt32();
                 modelLOD.MeshOffset = reader.ReadInt32();
                 modelLOD.SwitchPoint = reader.ReadSingle();
 
-                // Add the model LOD to the list
+                Console.WriteLine($"Model LOD {i}: NumMeshes = {modelLOD.NumMeshes}, MeshOffset = {modelLOD.MeshOffset}, SwitchPoint = {modelLOD.SwitchPoint}");
                 VTXModelLODs.Add(modelLOD);
             }
 
-            // return to original position
             reader.BaseStream.Seek(originalPos, SeekOrigin.Begin);
-
             return VTXModelLODs;
         }
 
@@ -143,21 +133,20 @@ namespace FileFormats.MDL.VTX
 
             for (int i = 0; i < ModelLODHeaders.Count; i++)
             {
-                reader.BaseStream.Seek(ModelLODHeaders[i].MeshOffset, SeekOrigin.Begin);
+                // Compute absolute offset: BodyPartOffset + ModelOffset + LodOffset + MeshOffset
+                long absoluteMeshOffset = Header.BodyPartOffset + BodyPartHeaders[i].ModelOffset + ModelHeaders[i].LodOffset + ModelLODHeaders[i].MeshOffset;
+                reader.BaseStream.Seek(absoluteMeshOffset, SeekOrigin.Begin);
 
                 VTXMeshHeader mesh = new VTXMeshHeader();
-
                 mesh.NumStripGroups = reader.ReadInt32();
                 mesh.StripGroupHeaderOffset = reader.ReadInt32();
                 mesh.Flags = (StripGroupFlags)reader.ReadByte();
 
-                // Add the mesh to the list
+                Console.WriteLine($"Mesh Header {i}: NumStripGroups = {mesh.NumStripGroups}, StripGroupHeaderOffset = {mesh.StripGroupHeaderOffset}, Flags = {mesh.Flags}");
                 VTXMeshes.Add(mesh);
             }
 
-            // return to original position
             reader.BaseStream.Seek(originalPos, SeekOrigin.Begin);
-
             return VTXMeshes;
         }
 
@@ -169,32 +158,28 @@ namespace FileFormats.MDL.VTX
 
             for (int i = 0; i < MeshHeaders.Count; i++)
             {
-                reader.BaseStream.Seek(MeshHeaders[i].StripGroupHeaderOffset, SeekOrigin.Begin);
-                VTXStripGroupHeader stripGroup = new VTXStripGroupHeader();
+                // Compute absolute offset: BodyPartOffset + ModelOffset + LodOffset + MeshOffset + StripGroupHeaderOffset
+                long absoluteStripGroupOffset = Header.BodyPartOffset + BodyPartHeaders[i].ModelOffset + ModelHeaders[i].LodOffset + ModelLODHeaders[i].MeshOffset + MeshHeaders[i].StripGroupHeaderOffset;
+                reader.BaseStream.Seek(absoluteStripGroupOffset, SeekOrigin.Begin);
 
+                VTXStripGroupHeader stripGroup = new VTXStripGroupHeader();
                 stripGroup.NumVerts = reader.ReadInt32();
                 stripGroup.VertOffset = reader.ReadInt32();
-
                 stripGroup.NumIndices = reader.ReadInt32();
                 stripGroup.IndexOffset = reader.ReadInt32();
-
                 stripGroup.NumStrips = reader.ReadInt32();
                 stripGroup.StripOffset = reader.ReadInt32();
-
                 stripGroup.Flags = (StripGroupFlags)reader.ReadByte();
 
                 // V49 Stuff
                 stripGroup.NumTopologyIndices = reader.ReadInt32();
                 stripGroup.TopologyOffset = reader.ReadInt32();
 
-                // Add the strip group to the list
+                Console.WriteLine($"Strip Group {i}: NumVerts = {stripGroup.NumVerts}, VertOffset = {stripGroup.VertOffset}, NumIndices = {stripGroup.NumIndices}, IndexOffset = {stripGroup.IndexOffset}, NumStrips = {stripGroup.NumStrips}, StripOffset = {stripGroup.StripOffset}, Flags = {stripGroup.Flags}");
                 VTXStripGroups.Add(stripGroup);
-
             }
 
-            // return to original position
             reader.BaseStream.Seek(originalPos, SeekOrigin.Begin);
-
             return VTXStripGroups;
         }
     }
