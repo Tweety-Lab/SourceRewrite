ac59c3df833a6ddbf5ad267ad0a371a8c79188bd
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-04-16 00:21:18 +1000

    Port "trigger_once" Entity.

---
+++ Engine/Entities/Trigger/TriggerOnce.cs
@@ -0,0 +1,24 @@
+﻿using SourceRewrite.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceRewrite.Entities
+{
+    [Entity("trigger_once")]
+    public class TriggerOnce : BaseTrigger
+    {
+        public override void Start()
+        {
+            base.Start();
+
+            PhysicsBody.OnCollisionStart += (entity) =>
+            {
+                FireOutput("OnStartTouch"); // Fire Output
+                DestroyDeferred(); // Destroy Self
+            };
+        }
+    }
+}
