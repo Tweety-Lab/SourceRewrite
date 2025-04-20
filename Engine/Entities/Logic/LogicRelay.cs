dfd850504048aa8e55685973cac44e24e5152c3e
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-04-20 10:08:05 +1000

    Port "logic_relay".

--- Engine/Entities/Logic/LogicRelay.cs
+++ Engine/Entities/Logic/LogicRelay.cs
@@ -10,11 +10,27 @@ namespace SourceRewrite.Entities.Logic
     [Entity("logic_relay")]
     public class LogicRelay : PointEntity
     {
+        public bool Enabled { get; set; } = true;
+
         [Input]
         public void Trigger()
         {
+            if (!Enabled) return;
+
             // Fire the trigger output
             FireOutput("OnTrigger");
+        }
+
+        [Input]
+        public void Disable()
+        {
+            Enabled = false;
+        }
+
+        [Input]
+        public void Enable()
+        {
+            Enabled = true;
         }
     }
 }
